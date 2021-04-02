637ade901ec5f515604363f8f58362a32ff8abf8
AnnuityDew <annuitydew@gmail.com>
2021-04-01 21:03:09 -0400

    Fixed GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,22 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../utils/gtag'
 import '../styles/global.css'
 
-// This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }) {
+// https://github.com/vercel/next.js/tree/canary/examples/with-google-analytics
+const App = ({ Component, pageProps }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
+
+export default App
